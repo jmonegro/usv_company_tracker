717abfa03771c40808e10d0a31cbc8e2232d59be
Prelang Builder <builder@prelang.com>
2014-08-01 16:42:16 +0000

    Made model 'Company' votable by adding acts_as_votable

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,4 +4,5 @@ class Company < ActiveRecord::Base
   validates_formatting_of :contact, using: :email
   validates_formatting_of :angel_list, using: :url
   validates_formatting_of :mattermark, using: :url
+  acts_as_votable 
 end
