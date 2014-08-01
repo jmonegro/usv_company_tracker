a531a2774fb232f868a19f60bf7944b5bf8faab0
Prelang Builder <builder@prelang.com>
2014-08-01 16:42:12 +0000

    Defined validation for attribute 'angel_list' on model 'Company'

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,5 @@ class Company < ActiveRecord::Base
   validates_formatting_of :website, using: :url
   validates_formatting_of :deck, using: :url
   validates_formatting_of :contact, using: :email
+  validates_formatting_of :angel_list, using: :url
 end
