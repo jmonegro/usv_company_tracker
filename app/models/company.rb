738a7f6d47bff486a13b90f444ba3b18e211cba5
Prelang Builder <builder@prelang.com>
2014-08-01 16:42:12 +0000

    Defined validation for attribute 'contact' on model 'Company'

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,5 @@
 class Company < ActiveRecord::Base
   validates_formatting_of :website, using: :url
   validates_formatting_of :deck, using: :url
+  validates_formatting_of :contact, using: :email
 end
